8c5af53d789da74daf18b6c38d1ec6f045d91725
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-05 09:39:52 +0600

    Add files via upload

---
+++ Chapter 3 - Data Types, Variables, and Arrays/Code_Example/Area.java
@@ -0,0 +1,11 @@
+class Area {
+    public static void main(String[] args) {
+        double pi, r, a;
+
+        r = 10.8; // Radius of circle
+        pi = 3.1416; // Approximate value of pi
+        a = pi * r * r; // Compute area
+
+        System.out.println("Area of circle is " + a);
+    }
+}
