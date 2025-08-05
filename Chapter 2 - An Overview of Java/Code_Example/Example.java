87376983ab848338e881e97585b0e4f15d600002
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-05 09:37:15 +0600

    Add files via upload

---
+++ Chapter 2 - An Overview of Java/Code_Example/Example.java
@@ -0,0 +1,16 @@
+/*
+ This is a simple Java program.
+*/
+
+class Example {
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
+/*
+public: Access modifier - callable from outside class
+static: Can be called without instantiating class
+void: Returns no value
+main: Method name (entry point)
+String[] args: Parameter for command-line arguments
+ */
