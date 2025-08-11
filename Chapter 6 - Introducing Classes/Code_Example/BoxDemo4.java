89967f10e03e638e41709e82641e1ba29d375bce
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-11 20:33:55 +0600

    Add files via upload

---
+++ Chapter 6 - Introducing Classes/Code_Example/BoxDemo4.java
@@ -0,0 +1,28 @@
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    double volume() 
+    {
+        return width * height * depth;
+    }
+}
+
+class BoxDemo4 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 30;
+
+        mybox2.width = 1;
+        mybox2.height = 2;
+        mybox2.depth = 3;
+
+        System.out.println("Volume is " + mybox1.volume());
+        System.out.println("Volume is " + mybox2.volume());
+    }
+}
